4ad14c8376e829fe0eb96de03fe7306552d0a180
Arnold Twala <arnoldtwl@gmail.com>
2023-09-24 13:32:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Caesar Cipher Project
+
+This program provides a utility to encrypt a message using the Caesar Cipher method.
+
+## Description
+
+The Caesar Cipher Project is a straightforward JavaScript program that runs in a browser. When the HTML page is loaded, the user is prompted to:
+
+1. Enter the message they want to encrypt.
+
+The program then encrypts the message using the Caesar Cipher method with a fixed shift value of 15 and displays the encrypted message in the browser's console.
+
+## Usage
+
+1. Open `cypher.html` in a web browser.
+2. Input the desired message for encryption when prompted.
+3. Check the browser's console to view the encrypted message.
+
+## Author
+
+- **Name**: Arnold Twala
+- **GitHub**: [https://github.com/eagleknite]
+
+## Contributing
+
+If you're interested in contributing, please fork the repository and use a feature branch. Pull requests are warmly welcome.
+
+## License
+
+This project is free software; you can redistribute it and/or modify it. Please refer to the license agreement for further details.
---
+++ cypher.js
@@ -0,0 +1,76 @@
+// Task: Ceasar Cypher Project
+// Author: Arnold Twala 
+
+
+
+// function for user input message
+function userMessage() {
+    let message = prompt("Enter the message you want to change into a cypher:");
+    return message;
+}
+
+// Define a constant "shift" and set its value to 15
+let shift = 15;
+// Define a function "encrypt" with a parameter "message":
+function encrypt(message) {
+    // Initialize an empty string called "encryptedMessage"
+    let encryptedMessage = "";
+    // Loop through each character "c" in the "message":
+    for (let c of message) {
+        // Check if "c" is an uppercase letter or a lowercase letter
+        // Use the "toUpperCase" and "toLowerCase" methods to check and compare "c" to its uppercase and lowercase versions
+        let isUpperCase = c.toUpperCase() === c && c.match(/[A-Z]/i);
+        let isLowerCase = c.toLowerCase() === c && c.match(/[a-z]/i);
+
+        // If "c" is an uppercase letter:
+        if (isUpperCase) {
+            // Convert "c" to its corresponding ASCII code
+            let asciiCode = c.charCodeAt();
+            // Add "shift" to the ASCII code
+            asciiCode += shift;
+            // If the result is greater than the ASCII code for 'Z', subtract 26 to keep it within the uppercase letter range
+            if (asciiCode > 90) {
+                asciiCode -= 26;
+            }
+            // Convert the updated ASCII code back to a character and append it to "encryptedMessage"
+            encryptedMessage += String.fromCharCode(asciiCode);
+        }
+
+        // Else if "c" is a lowercase letter:
+        else if (isLowerCase) {
+            // Convert "c" to its corresponding ASCII code
+            let asciiCode = c.charCodeAt();
+            // Add "shift" to the ASCII code
+            asciiCode += shift
+            // If the result is greater than the ASCII code for 'z', subtract 26 to keep it within the lowercase letter range
+            if (asciiCode > 122) {
+                asciiCode -= 26;
+            }
+            // Convert the updated ASCII code back to a character and append it to "encryptedMessage"
+            encryptedMessage += String.fromCharCode(asciiCode);
+        }
+
+        // Else(spaces, punctuation marks, etc.):
+        else {
+            // Append "c" directly to "encryptedMessage"
+            encryptedMessage += c;
+        }
+    }
+
+    // Return "encryptedMessage"
+    return encryptedMessage;
+}
+
+// Call the "encrypt" function with a sample message and store the result in a variable "encryptedMessage"
+let encryptedMessage = encrypt(userMessage());
+
+// Print out the encoded message
+console.log(encryptedMessage);
+alert(encryptedMessage);
+
+
+
+// References:
+// https://www.ascii-code.com/
+// https://en.wikipedia.org/wiki/Cipher
+// https://en.wikipedia.org/wiki/Caesar_cipher
